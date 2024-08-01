6b931038ddce18661fc1447b900afe00bfc4bf25
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-08-01 15:09:20 +0200

    dodana funkcja pomocnicza do pobrania listy składników

--- services/recipes.service.js
+++ services/recipes.service.js
@@ -1,1 +1,16 @@
 // services/recipes.service.js
+
+const Recipes = require("../models/recipe.model");
+const Ingredients = require("../models/ingredients.model");
+
+const fetchRecipe = (id) => {
+  return Recipes.findOne({
+    _id: id,
+  });
+};
+
+const fetchIngredients = () => {
+  return Ingredients.find({});
+};
+
+module.exports = { fetchRecipe, fetchIngredients };
